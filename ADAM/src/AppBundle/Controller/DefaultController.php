aaac663842bb918e94dcdf4109ea14abfa52dff2
Maxime REBIBO <maximerebibo@gmail.com>
2016-04-08 22:01:33 +0200

    Fix Register Routing

--- ADAM/src/AppBundle/Controller/DefaultController.php
+++ ADAM/src/AppBundle/Controller/DefaultController.php
@@ -91,7 +91,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/register", name="register")
+     * @Route("/register/", name="register")
      */
     public function registerAction(Request $request)
     {
