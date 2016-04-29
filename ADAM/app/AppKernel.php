cbf16aef26e1951c6fcbc70a3a34ca6aec1f4604
Maxime REBIBO <maximerebibo@gmail.com>
2016-04-29 02:03:18 +0200

    Création de l'AdminBundle

--- ADAM/app/AppKernel.php
+++ ADAM/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Mjm\KrumoBundle\MjmKrumoBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new AdminBundle\AdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
