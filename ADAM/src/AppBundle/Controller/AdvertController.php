649edb7ab58cf8720ba061e4a28b726883d510ff
Amandla <amandlabeautin@gmail.com>
2016-05-12 11:32:46 +0200

    Mise en place + ajout des plugins

--- ADAM/src/AppBundle/Controller/AdvertController.php
+++ ADAM/src/AppBundle/Controller/AdvertController.php
@@ -1,14 +1,18 @@
 <?php
  
  namespace AppBundle\Controller;
- 
+
+ use Symfony\Component\Form\AbstractType;
+ use Symfony\Component\Form\FormBuilder;
+ use Symfony\Component\Form\Extension\Core\Type\TextType;
  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
- use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  use Symfony\Component\HttpFoundation\Request;
- use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  use Symfony\Component\Form\FormBuilderInterface;
 
+ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+ use Ivory\CKEditorBundle\Form\Type\CKEditorType;
  
  class AdvertController extends Controller
  {
@@ -17,25 +21,31 @@
       */
      public function indexAction(Request $request)
      {
-     	$loginVariables = $this->get('user.security')->loginFormInstance($request);
-        $advertForm = $this->get('user_advert')->advertForm($request);
+	    // On crée le FormBuilder grâce au service form factory
+	    $formBuilder = $this->createFormBuilder()
+	    	->add('title', TextType::class, array(
+                "label" => "Titre",
+                "required" => true,
+            ))
+		    ->add('category', 'choice', array(
+			    'choices' => array(
+			        'Evenements' => 'Evenements',
+			        'Annonce emploi' => 'Annonce emploi'
+			    ),
+			    'required'    => true,
+			    'empty_value' => 'Choisissez votre catégorie',
+			    'empty_data'  => null
+			))
+        	->add('field', CKEditorType::class, array(
+	      		'label' => 'Corps',
+	            'config_name' => 'my_custom_config',
+        	))
+        	->getForm();
 
-         return $this->render('AppBundle::annonce.html.twig', array(
-            'form' => $registrationForm['form'],
-            'error' => $loginVariables['error'],
-            'csrf_token' => $loginVariables['csrf_token'],
-        ));
+	      // On passe la méthode createView() du formulaire à la vue
+	    // afin qu'elle puisse afficher le formulaire toute seule
+	    return $this->render('AppBundle::advert.html.twig', array(
+	      'form' => $formBuilder->createView(),
+	    ));
      }
-
-     public function advertForm(Request $request)
-    {
-        $form = $formFactory->createForm();
-        $form->setData($user);
-
-        $form->handleRequest($request);
-
-        return array(
-            'form' => $form->createView(),
-        );
-    }
  }
