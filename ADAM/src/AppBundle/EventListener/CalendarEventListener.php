905d1a8a9ecbc48300021c3edb3f418183cf9d74
Amandla <amandlabeautin@gmail.com>
2016-06-10 17:26:54 +0200

    Correction PR
    - creation requête Evénement

--- ADAM/src/AppBundle/EventListener/CalendarEventListener.php
+++ ADAM/src/AppBundle/EventListener/CalendarEventListener.php
@@ -18,7 +18,7 @@ class CalendarEventListener
 
     public function loadEvents(CalendarEvent $calendarEvent)
     {
-         $startDate = $calendarEvent->getStartDatetime();
+        $startDate = $calendarEvent->getStartDatetime();
         $endDate = $calendarEvent->getEndDatetime();
 
         // The original request so you can get filters from the calendar
@@ -30,9 +30,9 @@ class CalendarEventListener
 
         // load events using your custom logic here,
         // for instance, retrieving events from a repository
+        $repository = $this->entityManager->getRepository('AppBundle:Advert');
 
-         $evenements = $this->entityManager->getRepository('AppBundle:Advert')
-                            ->findByCategory(1);
+        $evenements = $repository->getAdvertByCategory('Événement');
         
         foreach($evenements as $evenement) {
             $id = $evenement->getId();
@@ -43,8 +43,6 @@ class CalendarEventListener
             $eventEntity->setAllDay(true); // default is false, set to true if this is an all day event
             $eventEntity->setBgColor('#BB0F47'); //set the background color of the event's label
             $eventEntity->setFgColor('#FFFFFF'); //set the foreground color of the event's label
-            //$eventEntity->setUrl('annonce/' . $id ); // url to send user to when event label is clicked
-            $eventEntity->setCssClass('my-custom-class'); // a custom class you may want to apply to event labels
             $eventEntity->addField('advertUrl', 'annonce/' . $id);
             
             //finally, add the event to the CalendarEvent for displaying on the calendar
